a8995e13aff59a6815eaf44f3e1e5a4063350c30
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-24 16:59:16 +0100

    The diff adds a number of imports, creates an express app, and uses the express.json(), express.cors(), and express.cookieParser() middleware. It also creates a MySQL connection and listens on port 8080.

--- server/server.js
+++ server/server.js
@@ -8,3 +8,29 @@ import jwt from 'jsonwebtoken';
 
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mydb'
+})
+//connect to the server and create a connection with mySQL
+db.connect((err) => {
+    if (err) {
+        console.log("Database connected");
+    }else{
+            console.log("Database is not connected");
+
+    }
+
+})
+
+
+app.listen(8080,()=>{
+    console.log('Server is running on port 8080');
+})
+
